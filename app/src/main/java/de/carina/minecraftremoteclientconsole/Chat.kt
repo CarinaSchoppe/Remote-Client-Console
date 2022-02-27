6573eeaaf16d46a61006afe4312f8aab2c4e71bf
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-02-27 21:21:43 +0100

    commit 1

---
+++ app/src/main/java/de/carina/minecraftremoteclientconsole/Chat.kt
@@ -0,0 +1,11 @@
+package de.carina.minecraftremoteclientconsole
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
